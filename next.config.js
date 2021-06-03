6baca09d2b2020a4650c2197e3ef552b0fe0558e
BernardA <bdealvarenga@gmail.com>
2021-06-03 16:56:28 +0200

    redirects, chg env prod

--- next.config.js
+++ next.config.js
@@ -6,19 +6,34 @@ module.exports = {
     async headers() {
         return [{ source: '/(.*)', headers: createSecureHeaders() }];
     },
-    /*
     async redirects() {
         // console.log('source', '/marques-voiture/:brand(\b([A-Z])(w+))');
         // console.log('destination', '/marques-voiture/:(l$1$2)');
         return [
+            /*
             {
                 source: '/marques-voiture/:brand([A-Za-z].*)',
                 destination: `/marques-voiture/:${brand.toLowerCase()}`,
                 permanent: false,
             },
+            */
+            {
+                source: '/prix-voiture/(.*)',
+                destination: '/prix-budget-voiture',
+                permanent: true,
+            },
+            {
+                source: '/segmentes-automobile/:segment(.*)',
+                destination: '/segments-automobile/:segment',
+                permanent: true,
+            },
+            {
+                source: '/comparatif-voiture/(.*)',
+                destination: '/comparatif-voiture',
+                permanent: true,
+            },
         ];
     },
-    */
     images: {
         domains: ['soeezauto-api', 'localhost', 'www.soeezauto.com'],
     },
